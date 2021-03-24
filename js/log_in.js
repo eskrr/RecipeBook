24caa5b6a0c27832de96517c49379b2b1e3ec684
eskrr <seesga97@gmail.com>
2021-03-23 22:39:53 -0600

    Fix

--- js/log_in.js
+++ js/log_in.js
@@ -1,6 +1,6 @@
   function validateForm() {
     if( $('#InputEmail').val() != "user@mail.com" || $('#InputPassword').val() != "password" ){
-        alert("Sign in must be filled out");
+        alert("Incorrect user");
         return false;
     }
   }
